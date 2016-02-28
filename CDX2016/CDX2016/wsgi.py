b0c48873bd157376a58d7c7941bdb1c497334ee4
Tom Parker <tjparker@hotmail.com>
2016-02-28 06:11:54 +0000

    Made django cookies more secure

--- CDX2016/CDX2016/wsgi.py
+++ CDX2016/CDX2016/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CDX2016.settings")
+os.environ['HTTPS'] = "on"
 
 application = get_wsgi_application()
